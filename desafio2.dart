8f23803ad7ad7761b5be08d3252ee09fceab0af3
Gabriel Aquino <92870223+gbrl08snts@users.noreply.github.com>
2023-04-03 16:01:58 -0300

    Create desafio2.dart

---
+++ desafio2.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  MaterialApp app = MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple), //Questão 1;
+      home: Scaffold(
+        appBar: AppBar(title: Text("Meu app")),
+        body: Center(
+            child: Column(children: [
+          Text(
+            "Apenas começando...",
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "No meio...",
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "Terminando...",
+            style: TextStyle(
+              color:Colors.green,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ])),
+        bottomNavigationBar: Text("Botão 1"),
+      ));
+
+  runApp(app);
+}
