0434d2b95078fe1e15f707cec6995b8e94ee51fe
Gabriel Aquino <92870223+gbrl08snts@users.noreply.github.com>
2023-04-03 16:23:57 -0300

    Update desafio2.dart

--- desafio2.dart
+++ desafio2.dart
@@ -1,36 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  MaterialApp app = MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepOrange), //Questão 1;
-      home: Scaffold(
-        appBar: AppBar(title: Text("Meu app")),
-        body: Center(
-            child: Column(children: [
-          Text( //Questão 2;
-            "Apenas começando...",
-            style: TextStyle(
-              color: Colors.red,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text( //Questão 2;
-            "No meio...",
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text( //Questão 2;
-            "Terminando...",
-            style: TextStyle(
-              color:Colors.green,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ])),
-        bottomNavigationBar: Text("Botão 1"),
-      ));
+  runApp(MyApp());
+}
 
-  runApp(app);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Meu App'),
+          backgroundColor: Colors.deepOrange, //Questão 1;
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                //Questão 2;
+                "Apenas começando...",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                //Questão 2;
+                "No meio...",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                //Questão 2;
+                "Terminando...",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Row(
+          //Questão 3;
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('Botão 1'),
+            Text('Botão 2'),
+            Text('Botão 3'),
+          ],
+        ),
+      ),
+    );
+  }
 }
