4ce2a73e4b8ff80e3b2c8fa833c401bc5b80cc8e
Gabriel Aquino <92870223+gbrl08snts@users.noreply.github.com>
2023-06-02 23:03:45 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'dart:convert';
 
 class DataService {
   static final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
@@ -18,74 +19,21 @@ class DataService {
     _carregarFunctions[index]();
   }
 
-  static void carregarCervejas() {
-    tableStateNotifier.value = [
-      {
-        "id": "163",
-        "uid": "978eee96-72f4-42b6-a221-bfe1c7376877",
-        "brand": "Becks",
-        "name": "Arrogant Bastard Ale",
-        "style": "Strong Ale",
-        "hop": "Horizon",
-        "yeast": "3333 - German Wheat",
-        "malts": "Munich",
-        "ibu": "72 IBU",
-        "alcohol": "8.5%",
-        "blg": "13.3°Blg"
-      },
-      {
-        "id": "8766",
-        "uid": "1732b5a9-211c-4789-8ccb-32fbc49c2bab",
-        "brand": "Kirin",
-        "name": "Hennepin",
-        "style": "Wood-aged Beer",
-        "hop": "Simcoe",
-        "yeast": "1187 - Ringwood Ale",
-        "malts": "Pale",
-        "ibu": "39 IBU",
-        "alcohol": "8.3%",
-        "blg": "5.8°Blg"
-      },
-      {
-        "id": "2969",
-        "uid": "12629ef8-c863-481f-b8b0-eedb543cb38d",
-        "brand": "Patagonia",
-        "name": "Westmalle Trappist Tripel",
-        "style": "European Amber Lager",
-        "hop": "Sterling",
-        "yeast": "2206 - Bavarian Lager",
-        "malts": "Vienna",
-        "ibu": "31 IBU",
-        "alcohol": "3.5%",
-        "blg": "9.8°Blg"
-      },
-      {
-        "id": "3559",
-        "uid": "6e86dc3e-e09b-4fdf-942d-66580c046e5f",
-        "brand": "Coors lite",
-        "name": "Alpha King Pale Ale",
-        "style": "Strong Ale",
-        "hop": "Equinox",
-        "yeast": "5733 - Pediococcus",
-        "malts": "Munich",
-        "ibu": "55 IBU",
-        "alcohol": "3.8%",
-        "blg": "12.8°Blg"
-      },
-      {
-        "id": "6799",
-        "uid": "1655b5bc-2b96-470b-a319-00e564fc827f",
-        "brand": "Quimes",
-        "name": "Delirium Noctorum",
-        "style": "Bock",
-        "hop": "Tettnang",
-        "yeast": "2124 - Bohemian Lager",
-        "malts": "Vienna",
-        "ibu": "50 IBU",
-        "alcohol": "2.3%",
-        "blg": "14.7°Blg"
-      }
-    ];
+  static Future<void> carregarCervejas() async {
+    var cervejaUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/beer/random_beer',
+        queryParameters: {'size': '5'});
+
+    var jsonString = await http.read(cervejaUri);
+    var cervejaJson = jsonDecode(jsonString) as List<dynamic>;
+    for (var cerveja in cervejaJson) {
+      cerveja['id'] = cerveja['id'].toString();
+    }
+
+    tableStateNotifier.value = cervejaJson;
+
     DataService.columnNames = [
       "ID",
       "UID",
@@ -114,54 +62,21 @@ class DataService {
     ];
   }
 
-  static void carregarCafes() {
-    tableStateNotifier.value = tableStateNotifier.value = [
-      {
-        "id": "9891",
-        "uid": "ed2109df-6a1b-439e-9f9c-0e9fd3ca3abb",
-        "blend_name": "Dark Town",
-        "origin": "Ilha do Café Gishamwana, Ruanda",
-        "variety": "Dilla",
-        "notes": "brilhante, com sabor de chá, borracha, tâmara, lúpulo",
-        "intensifier": "sem graça"
-      },
-      {
-        "id": "4871",
-        "uid": "fe96994a-4a3c-4fab-a1ed-f43a77e3eadf",
-        "blend_name": "Olhar de Ação de Graças",
-        "origin": "Mattari, Iêmen",
-        "variety": "S288",
-        "notes": "denso, sedoso, xarope de bordo, uva concord, carambola",
-        "intensifier": "vibrante"
-      },
-      {
-        "id": "6863",
-        "uid": "d448b0e9-66e9-433a-8a18-49a373ae7555",
-        "blend_name": "Copo de Especiarias de Abóbora",
-        "origin": "Chiriqui, Panamá",
-        "variety": "Pacas",
-        "notes": "crocante, intenso, canela, pão fresco, avelã",
-        "intensifier": "vibrante"
-      },
-      {
-        "id": "7704",
-        "uid": "e77bf8ef-9516-44e7-9cf0-e211572a2d28",
-        "blend_name": "Expresso Selecto",
-        "origin": "Montecillos, Honduras",
-        "variety": "Liberica",
-        "notes": "azedo, suave, chocolate ao leite, pão fresco, alcaçuz",
-        "intensifier": "suculento"
-      },
-      {
-        "id": "4525",
-        "uid": "742e8809-4f75-4ed2-a718-fc07cb9bc988",
-        "blend_name": "Bom-dia Estrela",
-        "origin": "Região de Mbeya, Tanzânia",
-        "variety": "Agaro",
-        "notes": "arredondado, escorregadio, melão, clementina, melaço",
-        "intensifier": "seco"
-      }
-    ];
+  static Future<void> carregarCafes() async {
+    var cafeUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/coffee/random_coffee',
+        queryParameters: {'size': '5'});
+
+    var jsonString = await http.read(cafeUri);
+    var cafeJson = jsonDecode(jsonString) as List<dynamic>;
+    for (var cafe in cafeJson) {
+      cafe['id'] = cafe['id'].toString();
+    }
+
+    tableStateNotifier.value = cafeJson;
+
     DataService.columnNames = [
       "ID",
       "UID",
@@ -182,54 +97,21 @@ class DataService {
     ];
   }
 
-  static void carregarNacoes() {
-    tableStateNotifier.value = [
-      {
-        "id": "9903",
-        "uid": "ec456dea-ca19-4e05-a31e-10f1a12d7b75",
-        "nationality": "Arubanos",
-        "idioma": "Português",
-        "capital": "Mogadíscio",
-        "esporte_nacional": "rúgbi",
-        "bandeira": "🇸🇾"
-      },
-      {
-        "id": "2549",
-        "uid": "9e197c3b-824e-4217-bf33-f57287fe4131",
-        "nationality": "Espanhóis",
-        "idioma": "Javanês",
-        "capital": "Damasco",
-        "esporte_nacional": "luta",
-        "bandeira": "🇧🇫"
-      },
-      {
-        "id": "3582",
-        "uid": "70c86b5a-bb50-4de7-95b0-44e8bac18e64",
-        "nationality": "Ingleses",
-        "idioma": "Italiano",
-        "capital": "Podgorica",
-        "esporte_nacional": "vôlei",
-        "bandeira": "🇧🇸"
-      },
-      {
-        "id": "2771",
-        "uid": "6e1117f8-3d20-4fea-b5c4-ee49a1a7c3e7",
-        "nationality": "Zimbabuanos",
-        "idioma": "Coreano",
-        "capital": "Majuro",
-        "esporte_nacional": "lacrosse",
-        "bandeira": "🇳🇮"
-      },
-      {
-        "id": "8408",
-        "uid": "c247a7bb-f833-4dca-a220-3155b51d243b",
-        "nationality": "Israelitas",
-        "idioma": "Coreano",
-        "capital": "Tashkent",
-        "esporte_nacional": "luta de óleo",
-        "bandeira": "🇸🇯"
-      }
-    ];
+  static Future<void> carregarNacoes() async {
+    var nacoesUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/nation/random_nation',
+        queryParameters: {'size': '5'});
+
+    var jsonString = await http.read(nacoesUri);
+    var nacoesJson = jsonDecode(jsonString) as List<dynamic>;
+    for (var nacoes in nacoesJson) {
+      nacoes['id'] = nacoes['id'].toString();
+    }
+
+    tableStateNotifier.value = nacoesJson;
+
     DataService.columnNames = [
       "ID",
       "UID",
@@ -243,10 +125,10 @@ class DataService {
       "id",
       "uid",
       "nationality",
-      "idioma",
+      "language",
       "capital",
-      "esporte_nacional",
-      "bandeira"
+      "national_sport",
+      "flag"
     ];
   }
 }
